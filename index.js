79a52181d5ee70dc15b725eb43c481f1c37af4e6
王镇 <hero21c@outlook.com>
2017-03-28 22:28:21 +0800

    新增 style

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 /**
- * Created by simon on 2017/3/15.
+ * Created by simon on 2017/3/22.
  */
+
+
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, } from 'react-native';
 
 export default class VisibleView extends Component {
 
   static propTypes = {
-    visible: React.PropTypes.bool
+    visible: React.PropTypes.bool,
+    style: React.PropTypes.object
   }
 
   render() {
     const comp = this.props.visible
-      ? <View>{this.props.children}</View>
+      ? <View style={this.props.style}>{this.props.children}</View>
       : null
 
     return comp
